b62346c98818b9c66c499e83801f187969475b36
explode-nut <1072215361@qq.com>
2024-09-04 22:55:27 +0800

    hw2 PercolationStats完成

--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,24 +1,62 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
 public class PercolationStats {
+    private final int N;
+    private final int T;
+    private final double[] xi;
     // perform T independent experiments on an N-by-N grid
     public PercolationStats(int N, int T, PercolationFactory pf) {
-
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.N = N;
+        this.T = T;
+        this.xi = new double[T];
+        for (int i = 0; i < T; i++) {
+            Percolation grid = pf.make(N);
+            int x = 0;
+            int[] permutation = StdRandom.permutation(N * N);
+            int index = 0;
+            while (!grid.percolates()) {
+                int[] rowAndCol = randomToXY(permutation[index]);
+                grid.open(rowAndCol[0], rowAndCol[1]);
+                x++;
+                index++;
+            }
+            xi[i] = x;
+        }
     }
+
+    /**
+     * 将随机位置转换为row，col
+     * @param random random为随机生成的grid中的位置
+     * @return result result中索引0为row，1为col
+     */
+
+    private int[] randomToXY(int random) {
+        int[] result = new int[2];
+        result[0] = random / N;
+        result[1] = random % N;
+        return result;
+    }
+
     // sample mean of percolation threshold
     public double mean() {
-        return 0;
+        return StdStats.mean(xi);
     }
     // sample standard deviation of percolation threshold
     public double stddev() {
-        return 0;
+        return StdStats.stddev(xi);
     }
     // low endpoint of 95% confidence interval
     public double confidenceLow() {
-        return 0;
+        return mean() - (1.96 * Math.sqrt(stddev()) / Math.sqrt(T));
     }
     // high endpoint of 95% confidence interval
     public double confidenceHigh() {
-        return 0;
+        return mean() + (1.96 * Math.sqrt(stddev()) / Math.sqrt(T));
     }
 }
