7d527b492b409b17114ed6fbf7695d75e4bbf81f
explode-nut <1072215361@qq.com>
2024-09-23 17:17:18 +0800

    lab11 Cycle Check完成

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+
 /**
  *  @author Josh Hug
  */
@@ -9,16 +12,69 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private Maze maze;
+    private boolean hasCycle;
+    private Stack<Integer> stack;
+    private int start; // 环的起点
+    private int end; // 环的终点
 
     public MazeCycles(Maze m) {
         super(m);
+        this.maze = m;
+        this.s = maze.xyTo1D(1, 1);
+        this.t = maze.xyTo1D(m.N(), m.N());
+        this.start = 0;
+        this.end = 0;
+        distTo[s] = 0;
+        this.hasCycle = false;
+        this.stack = new Stack<>();
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(s);
+        if (hasCycle) {
+            while (stack.peek() != start) {
+                Integer pop = stack.pop();
+                edgeTo[stack.peek()] = pop;
+                announce();
+            }
+            edgeTo[end] = start;
+            announce();
+        }
     }
 
     // Helper methods go here
+    private void dfs(int v) {
+        if (hasCycle) {
+            return;
+        }
+
+        marked[v] = true;
+        stack.push(v);
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (hasCycle) {
+                return;
+            }
+            if (!marked[w]) {
+                distTo[w] = distTo[v] + 1;
+                dfs(w);
+            } else {
+                if (distTo[w] != distTo[v] - 1) {
+                    hasCycle = true;
+                    start = w;
+                    end = v;
+                }
+                if (hasCycle) {
+                    return;
+                }
+            }
+        }
+
+    }
 }
 
